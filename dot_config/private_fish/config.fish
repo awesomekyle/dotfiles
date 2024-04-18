ca3fbebb5dd55e0cb61d332273581de9fc3c3d52
Kyle Weicht <303261+awesomekyle@users.noreply.github.com>
2024-04-18 16:30:29 -0400

    reenable symlinks; move path to start of config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,8 +2,8 @@ if not status is-interactive
     exit # Skip the file if non-interactive
 end
 
+fish_add_path "$HOME/.local/bin"
+
 set -gx ELECTRON_OZONE_PLATFORM_HINT auto
 set -gx REGISTRY_AUTH_FILE "$HOME/.podman-auth.json"
 set -gx EDITOR (type -p nvim)
-
-fish_add_path "$HOME/.local/bin:$HOME/bin"
