d6fdfb0b7743466aa826b0a0a8618851fad5702a
Kyle Weicht <303261+awesomekyle@users.noreply.github.com>
2024-04-18 15:32:14 -0400

    use proper syntax for fish vars

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,7 +2,8 @@ if not status is-interactive
     exit # Skip the file if non-interactive
 end
 
-set -g ELECTRON_OZONE_PLATFORM_HINT auto
-set -g REGISTRY_AUTH_FILE "$HOME/.podman-auth.json"
+set -gx ELECTRON_OZONE_PLATFORM_HINT auto
+set -gx REGISTRY_AUTH_FILE "$HOME/.podman-auth.json"
+set -gx EDITOR (type -p nvim)
 
 fish_add_path "$HOME/.local/bin:$HOME/bin"
