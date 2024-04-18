1403493f50fc13922fa44c70601542c0eb736ddd
Kyle Weicht <303261+awesomekyle@users.noreply.github.com>
2024-04-18 17:16:40 -0400

    add some ls aliases

--- dot_config/private_fish/conf.d/abbreviations.fish
+++ dot_config/private_fish/conf.d/abbreviations.fish
@@ -1,6 +1,7 @@
 abbr -a -- gco 'git checkout'
-# abbr -a -- ls eza
-# abbr -a -- la 'eza --all'
 abbr -a -- ip 'ip --color'
 abbr -a -- ipa 'ip --color addr'
 abbr -a -- vim nvim
+abbr -a -- ls eza --icons=auto
+abbr -a -- ll exa --icons=auto --group --header --group-directories-first --long
+abbr -a -- la exa --icons=auto --group --header --group-directories-first --long  --all --binary
