20b243a0a767f478b7f826c97bf657077bec68c8
Kyle Weicht <303261+awesomekyle@users.noreply.github.com>
2024-04-18 17:23:33 -0400

    turn `la` into a function

--- dot_config/private_fish/conf.d/abbreviations.fish
+++ dot_config/private_fish/conf.d/abbreviations.fish
@@ -2,6 +2,4 @@ abbr -a -- gco 'git checkout'
 abbr -a -- ip 'ip --color'
 abbr -a -- ipa 'ip --color addr'
 abbr -a -- vim nvim
-abbr -a -- ls eza --icons=auto
-abbr -a -- ll exa --icons=auto --group --header --group-directories-first --long
-abbr -a -- la exa --icons=auto --group --header --group-directories-first --long  --all --binary
+abbr -a -- ls eza
---
+++ dot_config/private_fish/functions/la.fish
@@ -0,0 +1,4 @@
+function la --wraps='exa --icons=auto --group --header --group-directories-first --long  --all --binary' --description 'alias la exa --icons=auto --group --header --group-directories-first --long  --all --binary'
+  exa --icons=auto --group --header --group-directories-first --long  --all --binary $argv
+        
+end
